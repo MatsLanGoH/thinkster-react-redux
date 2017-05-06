d21de8d488f6edcc248536f87173b3df4105c9a4
Matthias Lambrecht <mats.lan.pod@googlemail.com>
2017-05-06 23:36:03 +0900

    Dispatching Actions to change State

--- src/index.js
+++ src/index.js
@@ -26,12 +26,15 @@ class App extends React.Component {
   }
 
   render() {
+    const onClick = () => store.dispatch({ type: 'TOGGLE' });
+
     return (
       <div>
         <h1>To-dos</h1>
         <div>Learn Redux&nbsp;
           <input type="checkbox"
-                 checked={!this.state.checked} />
+                 checked={!this.state.checked}
+                 onClick={onClick} />
         </div>
         {
           this.state.checked ? (<h2>Done!</h2>) : null
