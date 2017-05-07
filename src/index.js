3baf8edb0c74cfbd49a9c18b4577b5a977f207ee
Matthias Lambrecht <mats.lan.pod@googlemail.com>
2017-05-07 22:58:28 +0900

    Implemented middlware

--- src/index.js
+++ src/index.js
@@ -3,17 +3,18 @@ import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import React from 'react';
 import { applyMiddleware, createStore } from 'redux';
+import { promiseMiddleWare } from './middleware';
 
 const defaultState = {
   appName: 'conduit',
   articles: null
 };
+
 const reducer = function(state = defaultState, action) {
   return state;
 };
 
-const store = createStore(reducer);
-
+const store = createStore(reducer, applyMiddleware(promiseMiddleWare));
 
 ReactDOM.render((
   <Provider store={store}>
